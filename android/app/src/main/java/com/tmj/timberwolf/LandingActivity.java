b99405c25eae108d699f11590d3466d89e2f1391
Joe Campbell <jcamp1095@gmail.com>
2018-02-13 15:49:22 -0500

    send sms code

--- android/app/src/main/java/com/tmj/timberwolf/LandingActivity.java
+++ android/app/src/main/java/com/tmj/timberwolf/LandingActivity.java
@@ -1,7 +1,9 @@
 package com.tmj.timberwolf;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.telephony.SmsManager;
+import android.widget.Toast;
 
 public class LandingActivity extends AppCompatActivity {
 
@@ -9,5 +11,27 @@ public class LandingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+        String phoneNumber;
+        String message;
+
+        phoneNumber = "+16178588563";
+        message = "test";
+        sendSMS(phoneNumber, message);
+    }
+
+    public void sendSMS(String phoneNo, String msg) {
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            //max 160 characters.  to send longer message:
+            //ArrayList<String> parts = smsManager.divideMessage(message);
+            //smsManager.sendMultipartTextMessage(phoneNumber, null, parts, null, null);
+            smsManager.sendTextMessage(phoneNo, null, msg, null, null);
+            Toast.makeText(getApplicationContext(), "Message Sent",
+                    Toast.LENGTH_LONG).show();
+        } catch (Exception ex) {
+            Toast.makeText(getApplicationContext(),ex.getMessage().toString(),
+                    Toast.LENGTH_LONG).show();
+            ex.printStackTrace();
+        }
     }
 }
