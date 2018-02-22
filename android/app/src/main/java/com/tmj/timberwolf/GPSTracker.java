74fda1f606c10d68600c49c94738b7555017b38f
Joe Campbell <jcamp1095@gmail.com>
2018-02-22 15:49:45 -0500

    works but gets stuck when turning on location

--- android/app/src/main/java/com/tmj/timberwolf/GPSTracker.java
+++ android/app/src/main/java/com/tmj/timberwolf/GPSTracker.java
@@ -8,26 +8,32 @@ import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
 import android.provider.Settings;
-import android.util.Log;
+
+//https://gist.github.com/ertugrulozcan/70973144b1c582618a6c
 
 /**
  * Created by tommy on 2/13/18.
  */
 
-public class GPSTracker extends Service implements LocationListener {
-
+public class GPSTracker extends Service implements LocationListener
+{
     private final Context mContext;
 
     Location location;
     double latitude;
     double longitude;
+    boolean isNetworkEnabled = false;
+    boolean canGetLocation = false;
+    boolean isGPSEnabled = false;
 
     // The minimum distance to change Updates in meters
     private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
 
     // The minimum time between updates in milliseconds
-    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1; // 1 minute
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60; // 1 minute
 
     // Declaring a Location Manager
     protected LocationManager locationManager;
@@ -39,6 +45,8 @@ public class GPSTracker extends Service implements LocationListener {
 
     public Location getLocation() {
         locationManager = (LocationManager) mContext.getSystemService(LOCATION_SERVICE);
+        isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
 
         // getting GPS status
         if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
@@ -46,6 +54,9 @@ public class GPSTracker extends Service implements LocationListener {
         }
 
         try {
+            if (isGPSEnabled && isNetworkEnabled) {
+                this.canGetLocation = true;
+            }
             if (location == null) {
                 locationManager.requestLocationUpdates(
                         LocationManager.GPS_PROVIDER,
@@ -63,9 +74,10 @@ public class GPSTracker extends Service implements LocationListener {
         } catch (SecurityException e) {
             e.printStackTrace();
         }
+        return location;
     }
 
-    private void showSettingsAlert() {
+    public void showSettingsAlert() {
         AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
 
         // Setting Dialog Title
@@ -95,4 +107,37 @@ public class GPSTracker extends Service implements LocationListener {
         // Showing Alert Message
         alertDialog.show();
     }
+
+    public boolean canGetLocation()
+    {
+        return this.canGetLocation;
+    }
+
+    @Override
+    public void onLocationChanged(Location location)
+    {
+    }
+
+    @Override
+    public void onProviderDisabled(String provider)
+    {
+    }
+
+    @Override
+    public void onProviderEnabled(String provider)
+    {
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras)
+    {
+    }
+
+    @Override
+    public IBinder onBind(Intent arg0)
+    {
+        return null;
+    }
+
+
 }
--- android/app/src/main/java/com/tmj/timberwolf/LandingActivity.java
+++ android/app/src/main/java/com/tmj/timberwolf/LandingActivity.java
@@ -1,22 +1,54 @@
 package com.tmj.timberwolf;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class LandingActivity extends AppCompatActivity {
+
+    private GPSTracker gpsTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
-        String phoneNumber;
-        String message;
+        gpsTracker = new GPSTracker(LandingActivity.this);
+        Button ShowLocationButton = findViewById(R.id.button);
 
-        phoneNumber = "+16178588563";
-        message = "test";
-        sendSMS(phoneNumber, message);
+        ShowLocationButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View arg0)
+            {
+                gpsTracker = new GPSTracker(LandingActivity.this);
+
+                if (gpsTracker.canGetLocation())
+                {
+                    Location loc = gpsTracker.getLocation();
+                    double longitude = loc.getLongitude();
+                    double latitude = loc.getLatitude();
+
+                    Toast.makeText(getApplicationContext(), "Location : \nLAT " + latitude + "\nLNG " + longitude, Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    // Konum bilgisi alinamiyorsa mesaj kutusunu goster
+                    gpsTracker.showSettingsAlert();
+                }
+            }
+        });
+
+
+//        String phoneNumber;
+//        String message;
+//
+//        phoneNumber = "+16178588563";
+//        message = "test";
+//        sendSMS(phoneNumber, message);
     }
 
     public void sendSMS(String phoneNo, String msg) {
