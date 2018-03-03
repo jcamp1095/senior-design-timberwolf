dfd5a3fff7a7492942636f3b558843addd7038bd
Michael Morisi <mcmorisi@gmail.com>
2018-03-03 11:32:21 -0500

    Initial commit

---
+++ run.py
@@ -0,0 +1,32 @@
+# /usr/bin/env python
+# Download the twilio-python library from twilio.com/docs/libraries/python
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+import googlemaps
+from datetime import datetime
+
+app = Flask(__name__)
+
+gmaps = googlemaps.Client(key='AIzaSyDREJYIfMrsNcZQCs09OalqjfHIdRsmHdA')
+
+@app.route("/sms", methods=['POST'])
+def sms_reply():
+
+    # Start our response
+    number = ""
+    message_body = ""
+    resp = MessagingResponse()
+
+    if request:
+        number = request.form['From']
+        message_body = request.form['Body']
+
+    # Add a message
+    resp.message("Ahoy! Thanks so much for your message.\n" +
+                 "Your Number is: " + number + "\nYour message was: " +  
+                 message_body)
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
