47263af23f4bdb247297b3f25a0a9dc495c37572
ctang <tommy9695@gmail.com>
2018-03-03 14:40:49 -0500

    added preliminary code for using the new location library

---
+++ android/app/src/main/java/com/tmj/timberwolf/LoadingActivity.java
@@ -0,0 +1,13 @@
+package com.tmj.timberwolf;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+    }
+}
